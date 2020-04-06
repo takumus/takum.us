e5b77206db654dba7c8f18df6508de0546d74cbd
takumus <me@takum.us>
2020-04-06 21:38:05 +0900

    あらき

--- .github/actions/push/index.js
+++ .github/actions/push/index.js
@@ -1,1 +1,21 @@
-console.log(process.env);
+const node_ssh = require("node-ssh");
+const path = require("path");
+const ssh = new node_ssh();
+ssh.connect({
+  port: process.env.DEPLOY_SERVER_PORT,
+  host: process.env.DEPLOY_SERVER_HOST,
+  username: process.env.DEPLOY_SERVER_USERNAME,
+  privateKey: process.env.DEPLOY_SERVER_PRIVATE_KEY
+})
+.then(async () => {
+  // remove dist
+  console.log("REMOVE!!!");
+  const rmResult = await ssh.execCommand("rm -rf ./takum.us/dist");
+  console.log('STDOUT: ' + rmResult.stdout);
+  console.log('STDERR: ' + rmResult.stderr);
+  // upload
+  console.log("UPLOAD!!!");
+  const uploadResult = await ssh.putDirectory(path.resolve(__dirname, "../../../dist"), "./takum.us/dist");
+  console.log('STDOUT: ' + uploadResult.stdout);
+  console.log('STDERR: ' + uploadResult.stderr);
+});
