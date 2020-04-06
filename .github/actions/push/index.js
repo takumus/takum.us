0f8c239e11d5dd1acc0d26eb115a2717a09e2562
takumus <me@takum.us>
2020-04-06 22:09:16 +0900

    サーバーディレクトリもsecretsへ。

--- .github/actions/push/index.js
+++ .github/actions/push/index.js
@@ -1,5 +1,5 @@
-const node_ssh = require("node-ssh");
-const path = require("path");
+const node_ssh = require('node-ssh');
+const path = require('path');
 const ssh = new node_ssh();
 ssh.connect({
   port: process.env.DEPLOY_SERVER_PORT,
@@ -8,19 +8,20 @@ ssh.connect({
   privateKey: process.env.DEPLOY_SERVER_PRIVATE_KEY
 })
 .then(async () => {
-  // remove dist
-  console.log("REMOVE!!!");
-  const rmResult = await ssh.execCommand("rm -rf ./takum.us/dist");
-  console.log('STDOUT: ' + rmResult.stdout);
-  console.log('STDERR: ' + rmResult.stderr);
-  // upload 1
-  console.log("UPLOAD1!!!");
-  const uploadResult = await ssh.putDirectory("./dist", "./takum.us/dist");
-  console.log('STDOUT: ' + uploadResult.stdout);
-  console.log('STDERR: ' + uploadResult.stderr);
-  // upload 2
-  console.log("UPLOAD2!!!");
-  const uploadResult2 = await ssh.putDirectory(path.resolve(__dirname,  "../../../dist"), "./takum.us/dist");
-  console.log('STDOUT: ' + uploadResult2.stdout);
-  console.log('STDERR: ' + uploadResult2.stderr);
+  // remove
+  console.log('remove');
+  const rmResult = await ssh.execCommand(`rm -rf ${process.env.DEPLOY_SERVER_DIRECTORY}`);
+  if (rmResult.stderr) {
+    console.log(rmResult.stderr);
+    process.exit(1);
+  }
+  // upload
+  console.log('upload');
+  const uploadResult = await ssh.putDirectory('./dist', process.env.DEPLOY_SERVER_DIRECTORY);
+  if (uploadResult.stderr) {
+    console.log(uploadResult.stderr);
+    process.exit(1);
+  }
+  // exit
+  process.exit(0);
 });
