8bb0600ae4bd4fa59c2b1ceba470ae31df8226b3
takumus <me@takum.us>
2020-04-12 11:07:54 +0900

    アプデ

--- src/scenes/P007Bezier.ts
+++ src/scenes/P007Bezier.ts
@@ -1,12 +1,15 @@
 import Scene from './scene';
 import * as THREE from "three";
 import { NumberType, ParamData } from '../params';
-import { Vector3, MeshNormalMaterial } from 'three';
+import { Vector3, MeshNormalMaterial, MeshPhongMaterial } from 'three';
 export class P007Bezier extends Scene {
   private scene: THREE.Scene;
   private camera: THREE.PerspectiveCamera;
+  private points: THREE.Vector3[] = [];
+  private time: number = 0;
+  private cylinder: THREE.Mesh;
   public get description() {
-    return "3D空間でもベジェの式が普通に使えることが分かった😀<br>ベジェな線を５本描いて連結した。<br>次はこれを動かしたりするぞ。";
+    return "3D空間でもベジェの式が普通に使えることが分かった😀<br>ベジェな線を連結した。ループするようにしてる。<br>次は頂点を動かしたりするぞ。";
   }
   constructor() {
     super();
@@ -14,57 +17,92 @@ export class P007Bezier extends Scene {
       70,   // fov
       1,    // aspect
       0.01, // near
-      10    // far
+      100    // far
     );
     this.scene = new THREE.Scene();
-    const rand = (n = 2) => (Math.random() - 0.5) * n;
-    const copy = (v: THREE.Vector3) => new THREE.Vector3(v.x, v.y, v.z);
-    let a = new THREE.Vector3(rand(1), rand(1), rand(1));
-    let b = new THREE.Vector3(rand(), rand(), rand());
-    for (let i = 0; i < 10; i++) {
-      const points: THREE.Vector3[] = [];
-      const c = new THREE.Vector3(rand(), rand(), rand());
-      const d = new THREE.Vector3(rand(1), rand(1), rand(1));
-      const getPos = (t: number) => {
-        const e = copy(b).sub(a).multiplyScalar(t).add(a);
-        const f = copy(d).sub(c).multiplyScalar(t).add(c);
-        const g = copy(c).sub(b).multiplyScalar(t).add(b);
-        const h = copy(g).sub(e).multiplyScalar(t).add(e);
-        const i = copy(f).sub(g).multiplyScalar(t).add(g);
-        const tp = copy(i).sub(h).multiplyScalar(t).add(h);
-        return tp;
-      }
-      for (let t = 0; t < 1; t += 0.01) {
-        points.push(getPos(t));
-      }
-      const line = new THREE.Line(
-        new THREE.BufferGeometry().setFromPoints(points),
-        new THREE.MeshBasicMaterial({color:0xffffff})
-      );
-      this.scene.add(line);
-      for (let t = 0; t <= 1; t += 0.2) {
-        const box = new THREE.Mesh(
-          new THREE.BoxGeometry(0.03, 0.03, 0.03),
-          new MeshNormalMaterial()
-        );
-        const tp = getPos(t);
-        const tp2 = getPos(t + 0.1);
-        box.position.set(tp.x, tp.y, tp.z);
-        box.lookAt(tp2);
-        this.scene.add(box);
-      }
-      a = d;
-      b = copy(c).sub(d).multiplyScalar(-1).add(d);
-    }
     this.scene.add(new THREE.AxesHelper(10));
     this.camera.position.z = 1;
     // attach
     this.currentScene = this.scene;
     this.currentCamera = this.camera;
+
+    this.cylinder = new THREE.Mesh(
+      new THREE.CylinderGeometry(0.03, 0.06, 0.2, 32),
+      new MeshPhongMaterial({
+        color: 0xCCCCCC,
+        shininess: 100,
+        specular: 0xffffff
+      })
+    );
+    this.cylinder.geometry.rotateX(Math.PI/2);
+    const light1 = new THREE.SpotLight(0x48FECB, 1, 100);
+    const light2 = new THREE.PointLight(0xFC7CB5, 1, 100);
+    light1.position.set(3, 3, 3);
+    light1.position.set(-3,-3,-3);
+    this.scene.add(light1, light2);
+    this.scene.add(this.cylinder);
+    // this.scene.add(new THREE.AmbientLight(0xffffff, 0.1));
+    this.drawBezierLine();
+  }
+  public drawBezierLine() {
+    const getPos = (t: number, a: THREE.Vector3, b: THREE.Vector3, c: THREE.Vector3, d: THREE.Vector3) => {
+      const ex = (b.x - a.x) * t + a.x;
+      const ey = (b.y - a.y) * t + a.y;
+      const ez = (b.z - a.z) * t + a.z;
+      const fx = (d.x - c.x) * t + c.x;
+      const fy = (d.y - c.y) * t + c.y;
+      const fz = (d.z - c.z) * t + c.z;
+      const gx = (c.x - b.x) * t + b.x;
+      const gy = (c.y - b.y) * t + b.y;
+      const gz = (c.z - b.z) * t + b.z;
+      const hx = (gx - ex) * t + ex;
+      const hy = (gy - ey) * t + ey;
+      const hz = (gz - ez) * t + ez;
+      const ix = (fx - gx) * t + gx;
+      const iy = (fy - gy) * t + gy;
+      const iz = (fz - gz) * t + gz;
+      const tx = (ix - hx) * t + hx;
+      const ty = (iy - hy) * t + hy;
+      const tz = (iz - hz) * t + hz;
+      return new THREE.Vector3(tx, ty, tz);
+    }
+    const rand = (n = 2) => (Math.random() - 0.5) * n;
+    const fa = new THREE.Vector3(rand(1), rand(1), rand(1));
+    const fb = new THREE.Vector3(rand(), rand(), rand());
+    let a = new THREE.Vector3(fa.x, fa.y, fa.z);
+    let b = new THREE.Vector3(fb.x, fb.y, fb.z);
+    const len = 20;
+    for (let i = 0; i < len; i++) {
+      let c = new THREE.Vector3(rand(), rand(), rand());
+      let d = new THREE.Vector3(rand(1), rand(1), rand(1));
+      if (i == len - 1) {
+        c = fb.sub(fa).multiplyScalar(-1).add(fa);
+        d = fa;
+      }
+      for (let t = 0; t < 1; t += 0.001) {
+        this.points.push(getPos(t, a, b, c, d));
+      }
+      a = d;
+      b = c.sub(d).multiplyScalar(-1).add(d);
+    }
+    this.points.push(fa);
+    const line = new THREE.Line(
+      new THREE.BufferGeometry().setFromPoints(this.points),
+      new THREE.MeshBasicMaterial({color:0xffffff})
+    );
+    this.scene.add(line);
   }
   public animate(deltaTime: number) {
-    this.scene.rotation.x += deltaTime * 0.001;
-    this.scene.rotation.y += deltaTime * 0.002;
+    this.time += deltaTime;
+    this.camera.position.z = Math.cos(this.time * 0.00001) * 0.5;
+    this.camera.position.x = Math.sin(this.time * 0.0003) * 0.5;
+    this.camera.position.y = Math.sin(this.time * 0.0002) * 1.4;
+    const ti = this.time * 0.5;
+    const cp = this.points[Math.floor(ti % this.points.length)];
+    const np = this.points[Math.floor((ti + 1) % this.points.length)];
+    this.cylinder.position.copy(cp);
+    this.cylinder.lookAt(np);
+    this.camera.lookAt(this.cylinder.position);
   }
   public resize(width: number, height: number) {
     this.camera.aspect = width / height;
