adaaa5c3c86dc936ee5dd10e3c65466fff71e16f
takumus <me@takum.us>
2020-04-14 03:38:28 +0900

    みずらいのを直した。

---
+++ src/components/Error.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <img alt="logo" src="../assets/logo.png" />
+    <h1>{{message}}</h1>
+  </div>
+</template>
+<style lang="scss" scoped>
+@import "../scss/utils.scss";
+img {
+  @include pc {
+    max-width: 256px;
+    min-width: 128px;
+    width: 30%;
+  }
+  @include sp {
+    width: 128px;
+  }
+  filter: grayscale(100%);
+  animation: rotating 1s linear infinite;
+}
+@keyframes rotating {
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
+}
+</style>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+@Component
+export default class Error extends Vue {
+  @Prop()
+  message!: string;
+  mounted() {
+    document.body.style.backgroundColor = "#DDD";
+  }
+  destroyed() {
+    document.body.style.backgroundColor = "";
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,56 +17,20 @@
   @include sp {
     width: 128px;
   }
-  -webkit-animation: rotating 5s linear infinite;
-  -moz-animation: rotating 5s linear infinite;
-  -ms-animation: rotating 5s linear infinite;
-  -o-animation: rotating 5s linear infinite;
   animation: rotating 5s linear infinite;
   animation-timing-function: cubic-bezier(0.68, -0.6, 0.32, 1.6);
-  -webkit-animation-timing-function: cubic-bezier(0.68, -0.6, 0.32, 1.6);
-  -moz-animation-timing-function: cubic-bezier(0.68, -0.6, 0.32, 1.6);
-  -o-animation-timing-function: cubic-bezier(0.68, -0.6, 0.32, 1.6);
   margin: 32px;
 }
 .github {
   width: 64px;
 }
-@-webkit-keyframes rotating {
-  from {
-    -webkit-transform: rotate(0deg);
-    -o-transform: rotate(0deg);
-    transform: rotate(0deg);
-  }
-  to {
-    -webkit-transform: rotate(360deg);
-    -o-transform: rotate(360deg);
-    transform: rotate(360deg);
-  }
-}
 @keyframes rotating {
-  from {
-    -ms-transform: rotate(0deg);
-    -moz-transform: rotate(0deg);
-    -webkit-transform: rotate(0deg);
-    -o-transform: rotate(0deg);
-    transform: rotate(0deg);
-  }
-  to {
-    -ms-transform: rotate(360deg);
-    -moz-transform: rotate(360deg);
-    -webkit-transform: rotate(360deg);
-    -o-transform: rotate(360deg);
-    transform: rotate(360deg);
-  }
+  from { transform: rotate(0deg); }
+  to { transform: rotate(360deg); }
 }
-
 </style>
 <script lang="ts">
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: "Home",
-  components: {}
-};
+import { Component, Vue } from "vue-property-decorator";
+@Component
+export default class Home extends Vue { }
 </script>
