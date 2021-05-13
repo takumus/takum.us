1fed30e322849db586947d20ad0cbb5371fdc38d
takumus <me@takumus.io>
2021-05-13 17:37:03 +0900

    ok

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,44 @@
-const path = require('path');
+// const path = require("path");
 module.exports = {
   chainWebpack: config => {
     config.module
-      .rule('ts')
-      .use('ts-loader')
+      .rule('vue')
+      .use('vue-loader')
       .tap(options => {
-        options.configFile = path.resolve(__dirname, "tsconfig.json");
-        return options;
-      })
-    
+        options.hotReload = false
+        return options
+      });
+    config.module
+      .rule('images')
+      .test(/\.(png)(\?.*)?$/)
+      .use('url-loader')
+      .options({
+        limit: 0,
+        name: `media/[name].[hash].[ext]`
+      });
+    config.module
+      .rule('media')
+      .test(/\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/)
+      .use('url-loader')
+      .options({
+        limit: 0,
+        name: `media/[name].[hash].[ext]`
+      });
   },
-  transpileDependencies: ['three', 'three-for-vue', 'cubic-bezier']
+  productionSourceMap: false,
+  pluginOptions: {
+    electronBuilder: {
+      nodeIntegration: true,
+      builderOptions: {
+        asar: true,
+        win: {
+          icon: "build/favicon.ico",
+          target: ["portable", "nsis"]
+        },
+        mac: {
+          icon: "build/logo.png"
+        }
+      }
+    }
+  }
 }
